98be36a9ecabc1fa5f21d379ed547817567a2759
Felipe <felipe.tavares@outlook.com>
2019-04-16 08:06:51 -0300

    Encapsulamento

---
+++ Construtores, palavra this, sobrecarga, encapsulamento/ConstrutoresEx02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutoresEx02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 999.00, 10);
+
+            p.SetNome("T");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco().ToString("F2", CultureInfo.InstalledUICulture));
+        }
+    }
+}
---
+++ Construtores, palavra this, sobrecarga, encapsulamento/ConstrutoresEx02/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConstrutoresEx02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConstrutoresEx02")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c43ba42b-c6ca-40ac-aeba-f52a5457c94b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
