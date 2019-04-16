5dc13783a62c99e53cded4b5d7b04ce2dbd5de99
Felipe Tavares <felipe.tavares@outlook.com>
2019-04-16 20:00:25 -0300

    Auto properties

---
+++ Construtores, palavra this, sobrecarga, encapsulamento/ConstrutoresEx04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutoresEx04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 999.00, 10);
+
+            p.Nome = "TV";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco.ToString("F2", CultureInfo.InstalledUICulture));
+        }
+    }
+}
