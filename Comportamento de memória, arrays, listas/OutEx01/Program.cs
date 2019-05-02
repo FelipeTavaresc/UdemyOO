7380aca7401c0f7e08f4c5ae885ca3d6d38ce097
Felipe <felipe.tavares@outlook.com>
2019-05-02 17:00:44 -0300

    Ex01 Out

---
+++ Comportamento de memória, arrays, listas/OutEx01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutEx01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+
+            Console.ReadKey();
+        }
+    }
+}
