0224033797eb8c5857084446cde5c464c5182f89
Felipe <felipe.tavares@outlook.com>
2019-05-24 10:13:53 -0300

    Initial Implementation

---
+++ Enumerações e composição/EnumAndComposition/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using EnumAndComposition.Entities.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace EnumAndComposition.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+
+        public Order (DateTime moment, OrderStatus orderStatus)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            OrderItem = new List<OrderItem>();
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItem.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItem.Remove(item);
+        }
+    }
+}
