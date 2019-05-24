5317dee8f02e2acb2723aa5c58ac25c1f08431e1
Felipe <felipe.tavares@outlook.com>
2019-05-24 17:22:09 -0300

    ...

--- Herança e polimorfismo/HerancaEx01/Program.cs
+++ Herança e polimorfismo/HerancaEx01/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HerancaEx01.Entities;
+using System;
 
 namespace HerancaEx01
 {
@@ -10,6 +7,37 @@ namespace HerancaEx01
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(101, "Alex", 0.0);
+            BussinessAccount bacc = new BussinessAccount(1002, "Maria", 0.0, 500.00);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BussinessAccount(1003, "Bob", 0.0, 200.00);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            BussinessAccount acc4 = (BussinessAccount)acc3;
+            acc4.Loan(100.0);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            if (acc3 is BussinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BussinessAccount acc5 = acc3 as BussinessAccount;
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
