1dafae0da1248659e7850658e858f4943bc398ad
Felipe <felipe.tavares@outlook.com>
2019-05-31 08:03:44 -0300

    Métodos Abstratos Ex01

--- Herança e polimorfismo/MetodosAbstratosEx01/Entities/Rectangle.cs
+++ Herança e polimorfismo/MetodosAbstratosEx01/Entities/Rectangle.cs
@@ -12,10 +12,10 @@ namespace MetodosAbstratosEx01.Entities
         public double Width { get; set; }
         public double Height { get; set; }
 
-        public Rectangle(double width, double heitht, Color color) : base (color)
+        public Rectangle(double width, double height, Color color) : base (color)
         {
             Width = width;
-            Height = heitht;
+            Height = height;
         }
         public override double Area()
         {
