de477c3f4095eb5bcd919e7dfd749a9ac2eb1962
Felipe <felipe.tavares@outlook.com>
2019-06-04 15:57:11 -0300

    Começando a implementar o métedo que imprimi o tabuleiro

--- JogoXadrez/xadrez-console/Program.cs
+++ JogoXadrez/xadrez-console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,7 +7,13 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
